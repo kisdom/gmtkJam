29f37378df1406fcea2eb4403bdcb98cc483d951
Denes <denes.godolle@gmail.com>
2026-07-25 17:30:35 +0200

    cancel action with space

--- gmtk-jam/Scripts/map.gd
+++ gmtk-jam/Scripts/map.gd
@@ -29,9 +29,9 @@ func _ready() -> void:
 	
 func _unhandled_input(event: InputEvent) -> void:
 	# Mégse akció jobb egérgombbal (vagy ESC-pel)
-	# if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-	# 	_cancel_current_action()
-	# 	return
+	if Input.is_action_just_pressed("Space"):
+		_cancel_current_action()
+		return
 
 	# --- VÉDELMI VONAL RAJZOLÁS (2 kattintásos logika) ---
 	if state == State.DEFENSE and active_defense_line != null:
