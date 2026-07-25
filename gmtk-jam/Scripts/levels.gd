850cd86fc3a408335f76c47e51b8acdb58f2eeaf
Denes <denes.godolle@gmail.com>
2026-07-25 22:05:51 +0200

    ordering

--- gmtk-jam/Scripts/levels.gd
+++ gmtk-jam/Scripts/levels.gd
@@ -51,3 +51,7 @@ func _load_level(level_name: String):
 	for base in bases.get_children():
 		base.show() 
 		base.disabled = false
+
+	if level_node.has_node("Sprite2D"):
+		var sprite = level_node.get_node("Sprite2D")
+		sprite.show()
