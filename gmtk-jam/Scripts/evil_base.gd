201866d27eca2808af07748a78e0d2c1eba5e90d
kisdom <kissdominik@gmail.com>
2026-07-25 22:01:05 +0200

    Merge fixes

--- gmtk-jam/Scripts/evil_base.gd
+++ gmtk-jam/Scripts/evil_base.gd
@@ -6,7 +6,7 @@ signal action_requested(base_node, action_type)
 
 
 var isPressed: bool = false
-var isEvil: bool = false
+var isEvil: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,31 +15,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 		pass
-
-func _on_pressed() -> void:
-	isPressed = not isPressed
-	if isPressed:
-		panel_container.visible = true
-	else:
-		panel_container.visible = false
-
-func _on_defense_pressed() -> void:
-	panel_container.hide()
-	SignalBus.prepare_defense.emit(self)
-
-func _on_rocket_pressed() -> void:
-	panel_container.hide()
-	SignalBus.send_rocket.emit(self)
-
-func _on_radar_pressed() -> void:
-	panel_container.hide()
-	SignalBus.radar_search.emit(self)
 	
 func _base_destruction() -> void:
 	print(name + " destroyed")
 	texture_normal = load("res://gmtk-jam/resources/fire.jpg")
 	scale = Vector2(1,1)
 	print(size)
-	$PanelContainer.visible = false
 	disabled = true
 	SignalBus.base_destroyed.emit(self)
