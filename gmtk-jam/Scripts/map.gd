7ae98ecf11044ea179385560a7fcc0ed883ee2c0
kisdom <kissdominik@gmail.com>
2026-07-25 09:09:33 +0200

    reggelt

--- gmtk-jam/Scripts/map.gd
+++ gmtk-jam/Scripts/map.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	for base in get_tree().get_nodes_in_group("bases"):
+		base.action_requested.connect(_on_base_action_requested)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +14,4 @@ func _process(delta: float) -> void:
 
 
 func _on_base_action_requested(base_node: Variant, action_type: Variant) -> void:
-	pass # Replace with function body.
+	print(base_node.name)
