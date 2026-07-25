5b12d26f362b6f8fd872a38b9493662f1696891d
Denes <denes.godolle@gmail.com>
2026-07-25 22:09:18 +0200

    fix

--- gmtk-jam/Scripts/end_of_game.gd
+++ gmtk-jam/Scripts/end_of_game.gd
@@ -12,14 +12,14 @@ func _ready():
 func setup():
 	var bases_node = map.get_child(0).get_node("Bases")
 	for base in bases_node.get_children():
-		if base.is_evil:
+		if base.isEvil:
 			enemy_base_count += 1
 		else:
 			friendly_base_count += 1
 
 
 func _base_destroyed(base_node):
-	if base_node.is_evil:
+	if base_node.isEvil:
 		enemy_base_count -= 1
 		if enemy_base_count == 0:
 			SignalBus.endig.emit("win")
