908bcc818a62d7f01d226b1baf54edebd1927b29
Denes <denes.godolle@gmail.com>
2026-07-26 11:36:23 +0200

    base fix

--- gmtk-jam/Scripts/evil_base.gd
+++ gmtk-jam/Scripts/evil_base.gd
@@ -8,18 +8,42 @@ signal action_requested(base_node, action_type)
 var isPressed: bool = false
 var isEvil: bool = true
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	_set_random_base_image()
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-		pass
+func _set_random_base_image() -> void:
+	# Fontos: Ellenőrizd a pontos útvonalat! Lehet, hogy "res://gmtk-jam/resources/bases/base/" kell neked.
+	var folder_path = "res://gmtk-jam/resources/bases/enemy_base/"
+	
+	# Lekérjük a mappa tartalmát
+	var files = DirAccess.get_files_at(folder_path)
+	var valid_images = []
+	
+	for file in files:
+		# Leszedjük a .import kiterjesztést, ha exportált buildről van szó
+		var clean_file = file.replace(".import", "")
+		
+		# Csak a képfájlokat engedjük be a listába
+		if clean_file.ends_with(".png") or clean_file.ends_with(".jpg") or clean_file.ends_with(".webp"):
+			if not valid_images.has(clean_file):
+				valid_images.append(clean_file)
+				
+	# Ha találtunk képet, véletlenszerűen választunk egyet
+	if valid_images.size() > 0:
+		var random_image = valid_images.pick_random()
+		
+		# 1. Rátöltjük a képet a Sprite2D-re a gomb textúrája helyett!
+		$Sprite2D.texture = load(folder_path + random_image)
+		
+	else:
+		print("Hiba: Nem találtam képeket ebben a mappában: " + folder_path)
 	
 func _base_destruction() -> void:
 	print(name + " destroyed")
-	texture_normal = load("res://gmtk-jam/resources/fire.jpg")
-	scale = Vector2(1,1)
-	print(size)
-	disabled = true
+	
+	# 1. Rátöltjük a tüzet a Sprite2D-re! 
+	# (Mivel a Sprite2D középre van igazítva, a mérettől függetlenül jó helyen lesz)
+	$Sprite2D.texture = load("res://gmtk-jam/resources/fire.png")
+	
+	# Ha van SignalBus bekötésed (a korábbi kódjaid alapján), azt is elsütjük
 	SignalBus.base_destroyed.emit(self)
